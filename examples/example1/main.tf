1a2751665d12dfc113272ae0beddbab02c0bcd6e
vusal-iom <vusal@iomete.com>
2024-04-24 17:28:56 +0200

    Update example deployment comments

--- examples/example1/main.tf
+++ examples/example1/main.tf
@@ -1,15 +1,12 @@
-# Pre-requisites:
-# 1. Create a GCP project. See: https://cloud.google.com/resource-manager/docs/creating-managing-projects
-# 2. Create a Cloud Storage bucket in the project (pay attention to the location of the bucket, it should be the same as the location of the cluster)
-
 module "iomete-data-plane" {
   source  = "iomete/iomete-data-plane/gcp"
   version = "1.1.0"
 
-  # A unique cluster name for IOMETE. It should be unique within GCP project and compatible with GCP naming conventions (See: https://cloud.google.com/compute/docs/naming-resources)
+  # A user provided cluster name for IOMETE. It should be unique within GCP project and compatible with GCP naming conventions (See: https://cloud.google.com/compute/docs/naming-resources)
   cluster_name = "my-lakehouse-cluster"
 
   # Google Cloud project ID. This is a unique identifier for your project and can be found in the Google Cloud Console. Recommended to create a new project for IOMETE.
+  # To create a GCP project. See: https://cloud.google.com/resource-manager/docs/creating-managing-projects
   project_id = "iom-prj1"
 
   # The region where the cluster and Cloud storage will be hosted
@@ -18,7 +15,7 @@ module "iomete-data-plane" {
   # The zone where the cluster will be hosted?
   zone = "us-central1-c"
 
-  # Create a new Bucket in your project and provide the name here
+  # Create a Cloud Storage bucket inside the GCP the project (pay attention to the location of the bucket, it should be the same as the location of the cluster) and provide the name here
   lakehouse_storage_bucket_name = "iom-lakehouse-bucket1"
 }
 
